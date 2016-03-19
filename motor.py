c0474d80fdeaf5083631992b184f7d264bfe7182
Darshan Rai <darshanjrai@gmail.com>
2016-03-19 21:14:20 +0530

    Clean up motor class
    Use variables for max and min powers.
    
    Remove pin format since it is checked at a lower level anyway.
    
    Instead of logging and not setting power when it is out of bounds,
    simply cap it to the bound.
    
    Remove the destructor. There is no guarantee that it will be called
    it is best to simply use PWM.cleanup()

--- motor.py
+++ motor.py
@@ -3,31 +3,28 @@ from scipy.interpolate import interp1d
 import logging
 
 class motor:
-    power = 0.0
+    MAX_POWER = 100
+    MIN_POWER = 0
 
     def __init__(self, pin):
         self.pin = pin
-        if type(pin) is not str:
-            logging.info("Invalid pin given: %s", pin)
-            exit()
+        self.power = MIN_POWER
         # start motor with power = 0 (5 on the scale of [5, 10])
         PWM.start(self.pin, 5, 50)
 
     def setPower(self, power):
-        if power > 100 or power < 0 :
-            logging.info("Value entered %d is not valid", power)
-            return
-        self.power = power
+        if power > MAX_POWER:
+            self.power = MAX_POWER
+        elif power < MIN_POWER:
+            self.power = MIN_POWER
+        else:
+            self.power = power
+
         # map values from [0,100] to [5,10].
         # A frequency of 50Hz translates to a period of 20ms
         # pulse of 1ms is no power, pulse of 2ms is full power.
-        power = interp1d([0,100],[5.0,10.0])(power)
+        power = interp1d([MIN_POWER, MAX_POWER], [5.0, 10.0])(power)
         PWM.set_duty_cycle(self.pin, power)
 
     def getPower(self):
         return self.power
-
-    def __del__(self):
-        # stop motor with power = 0 (5 on the scale of [5, 10])
-        PWM.set_duty_cycle(self.pin, 5)
-        PWM.stop(self.pin)

957153fd7e05064f4e9d6fabd33f1503c05629a9
Karthik Nayak <Karthik.188@gmail.com>
2016-03-23 19:01:42 +0530

    control: fix the problem of angles switching from +/- 179 to -/+ 179
    Helped-by: Payal Mehta <Mehta.Payal.2794@gmail.com>
    Signed-off-by: Darshan J Rai <DarshanJRai@gmail.com>
    Signed-off-by: Karthik Nayak <Karthik.188@gmail.com>

--- control.py
+++ control.py
@@ -96,6 +96,9 @@ def updateMotors(mpuAngles):
     op = {}
     for i in mpuAngles:
         error = pid[i[0]].SetPoint - mpuAngles[i]
+        # address the issue where the angle switches from +/- 179 to +/- 179.
+        if 360 - error < error:
+            error = - (360 - error)
         pid[i[0]].update(error)
         op[i] = pid[i[0]].output
 

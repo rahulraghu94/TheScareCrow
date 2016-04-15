96d85bd41c30dbe6a269433ff093a07ad7b95d0d
Darshan Rai <darshanjrai@gmail.com>
2016-04-15 11:41:14 +0530

    radio: fix radio class
    After testing and looking at other implementation, putting in the
    address plus static payload size proved to be reliable.

--- radio.py
+++ radio.py
@@ -5,15 +5,17 @@ class Radio:
     # Only the first byte needs to be different. We can open 6 pipes
     # simultaneously for reading (0-5), but the pipe 0 is used for
     # writing also. Only one pipe can be used for writing.
-    pipes = ["1Node", "2Node"]
+    # pipes = ["1Node", "2Node"]
+    pipes = [0x65646f4e32, 0x65646f4e31]
+    payloadSize = 32
 
-    def __init__(self, channel=1, pa_level=RF24_PA_MAX, ce_pin=49):
+    def __init__(self, channel=0x0A, pa_level=RF24_PA_MAX, ce_pin=49):
         self.r = RF24(ce_pin, 0)
         self.r.begin()
+        self.r.setRetries(5,15)
         self.r.setChannel(channel)
         self.r.setPALevel(pa_level)
-        self.r.enableDynamicPayloads()
-        self.r.setRetries(5,15)
+        self.r.payloadSize = self.payloadSize
         self.r.openWritingPipe(self.pipes[0]);
         self.r.openReadingPipe(1, self.pipes[1]);
         self.r.startListening();
@@ -25,7 +27,7 @@ class Radio:
         if self.r.available():
             data = []
             while self.r.available():
-                len = self.r.getDynamicPayloadSize()
+                len = self.payloadSize
                 data.append(self.r.read(len).decode('utf-8'))
             return data
         else:
